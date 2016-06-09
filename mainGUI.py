93a4e219134ae711b7227595782df10107d29c9a
Ramakanta Chandra <ramakantac@gmail.com>
2016-06-09 19:30:48 +0530

    Add files via upload

--- mainGUI.py
+++ mainGUI.py
@@ -1,7 +1,7 @@
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
-import GSMUtility, sys
+import GSMUtility_New, sys
 import sys
 import linecache
 import serial
@@ -27,7 +27,12 @@ port_no = ''
 tcp_data = ''
 source_address = ''
 get_post = ''
-new_apn = ''
+tu_veriable=''
+ftp_server_name=''
+user_name=''
+ftp_password=''
+ftp_file_name=''
+ftp_directry=''
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
@@ -50,7 +55,7 @@ except AttributeError:
         return s
 
 
-class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
+class MainGUIClass(QtGui.QMainWindow, GSMUtility_New.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainGUIClass, self).__init__(parent)
         self.setupUi(self)
@@ -85,7 +90,17 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
         QtCore.QObject.connect(self.pushButton_5, QtCore.SIGNAL(_fromUtf8("clicked()")), self.post_fun)
         QtCore.QObject.connect(self.lineEdit_16, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.http_apn)
         QtCore.QObject.connect(self.pushButton_26, QtCore.SIGNAL(_fromUtf8("clicked()")), self.session_close)
-
+        QtCore.QObject.connect(self.comboBox, QtCore.SIGNAL(_fromUtf8("activated(QString)")), self.tcp_udp)
+        QtCore.QObject.connect(self.lineEdit_6, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.ftp_server)
+        QtCore.QObject.connect(self.lineEdit_7, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.username)
+        QtCore.QObject.connect(self.lineEdit_8, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.password)
+        QtCore.QObject.connect(self.lineEdit_9, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.file_name)
+        QtCore.QObject.connect(self.lineEdit_10, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.directry)
+        QtCore.QObject.connect(self.lineEdit_11, QtCore.SIGNAL(_fromUtf8("textChanged(QString)")), self.ftp_apn)
+        QtCore.QObject.connect(self.pushButton_9, QtCore.SIGNAL(_fromUtf8("clicked()")), self.ftp_push)
+        QtCore.QObject.connect(self.pushButton_10, QtCore.SIGNAL(_fromUtf8("clicked()")), self.ftp_get)
+        QtCore.QObject.connect(self.pushButton_11, QtCore.SIGNAL(_fromUtf8("clicked()")), self.ftp_connect)
+        QtCore.QObject.connect(self.pushButton_12, QtCore.SIGNAL(_fromUtf8("clicked()")), self.ftp_disconnect)
     def port_update(self):
         try:
             self.portComboBox.clear()
@@ -148,24 +163,22 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
 
     def send_script(self):
         global ScriptData
-        # try:
-
         GSM_port.write(str(ScriptData) + "\r\n")
-        # except:
-        #     print "Send Failed"
-
-    def CallText(self):
-
-        global number
-        GSM_port.write('ATD' + str(number) + ';' + "\r\n")
-
 
     def callno(self, data):
         global number
         number = data
 
+    def CallText(self):
+          global number
+          GSM_port.write('ATD' + str(number) + ';' + "\r\n")
+
+
+
+
     def end_call(self):
         GSM_port.write('ATH' + "\r\n")
+
 
     def smsno(self, data1):
         global smsnum
@@ -185,7 +198,6 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
         GSM_port.write(str(self.plainTextEdit.toPlainText()) + chr(26))
 
     def accept_call(self):
-        global Console_Data
         GSM_port.write('ATA' + chr(13))
 
     def decline(self):
@@ -195,8 +207,8 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
       global c
       global Console_Data
       if c!=0 :
-        self.lineEdit.setText(str(Console_Data[c:c+12]))
-        if Console_Data[c+31:c+41]=='NO CARRIER' or Console_Data[c+29:c+32]:
+        self.lineEdit.setText(str(Console_Data[c:c+13]))
+        if Console_Data[c+31:c+41]=='NO CARRIER' or Console_Data[c+29:c+32]=='ADH':
              self.lineEdit.setText('')
 
     def get_apn(self,data2):
@@ -212,21 +224,41 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
         port_no=data4
 
 
-    def connect_gprs(self):
+    def connect_gprs(self,data5):
          global apn
          global server_ip
          global port_no
-         global Console_Data
-         Console_Data='Please Wait Device is Getting Connected...\n'
-         GSM_port.write('AT+CIPMUX=0'+ chr(13))
-         time.sleep(1)
-         GSM_port.write('AT+CSTT='+'"'+str(apn)+'"' + chr(13))
-         time.sleep(1)
-         GSM_port.write('AT+CIICR'+ chr(13))
-         time.sleep(1)
-         GSM_port.write('AT+CIFSR'+ chr(13))
-         time.sleep(1)
-         GSM_port.write('AT+CIPSTART="TCP"'+',''"'+str(server_ip)+'"'+',''"'+str(port_no)+'"'+chr(13))
+         global tu_veriable
+         tu_veriable=data5
+         # try:
+         if tu_veriable=='TCP':
+             Console_Data='Please Wait Device is Getting Connected...\n'
+             GSM_port.write('AT+CIPMUX=0'+ chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CSTT='+'"'+str(apn)+'"' + chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CIICR'+ chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CIFSR'+ chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CIPSTART="TCP"'+',''"'+str(server_ip)+'"'+',''"'+str(port_no)+'"'+chr(13))
+
+         elif tu_veriable=='UDP':
+             Console_Data='Please Wait Device is Getting Connected...\n'
+             GSM_port.write('AT+CGATT?'+ chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CSTT='+'"'+str(apn)+'"' + chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CIICR'+ chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CIFSR'+ chr(13))
+             time.sleep(1)
+             GSM_port.write('AT+CIPSTART="UDP"'+',''"'+str(server_ip)+'"'+',''"'+str(port_no)+'"'+chr(13))
+
+         # except:
+         #     Console_Data='Please Select either TCP or UDP from the Drop Down Menu'
+         #     self.SerialConsole.setPlainText(Console_Data)
+
 
     def send_data(self,data5):
         global tcp_data
@@ -234,6 +266,7 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
 
     def send_button(self):
         global tcp_data
+        global tu_veriable
         GSM_port.write('AT+CIPSEND'+ chr(13))
         time.sleep(1)
         GSM_port.write(str(self.plainTextEdit_7.toPlainText()) + chr(26))
@@ -247,17 +280,17 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
         get_post=data7
 
     def http_apn(self,data8):
-        global new_apn
-        new_apn=data8
+        global apn
+        apn=data8
 
     def get_fun(self):
         global source_address
         global get_post
         global new_apn
         GSM_port.write('AT+SAPBR=3,1,"Contype","GPRS"'+chr(13))
-        time.sleep(1)
+        # time.sleep(1)
         GSM_port.write('AT+SAPBR=3,1,"APN",'+'"'+str(new_apn)+'"'+chr(13))
-        time.sleep(1)
+        # time.sleep(1)
         GSM_port.write('AT+SAPBR=1,1'+chr(13))
         time.sleep(1)
         GSM_port.write('AT+SAPBR=2,1'+chr(13))
@@ -267,14 +300,16 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
         GSM_port.write('AT+HTTPINIT'+chr(13))
         time.sleep(1)
         GSM_port.write('AT+HTTPPARA="CID",1'+chr(13))
-        time.sleep(1)
+        # time.sleep(1)
         GSM_port.write('AT+HTTPPARA="URL",'+'"'+str(self.plainTextEdit_2.toPlainText())+'"'+chr(13))
         time.sleep(1)
         GSM_port.write('AT+HTTPACTION=0'+chr(13))
-        time.sleep(1)
-        # GSM_port.write('AT+HTTPDATA'+chr(13))
+        # time.sleep(1)
+        # # GSM_port.write('AT+HTTPDATA'+chr(13))
         time.sleep(1)
         GSM_port.write('AT+HTTPREAD'+chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+HTTTERM'+chr(13))
 
 
     def post_fun(self):
@@ -295,12 +330,107 @@ class MainGUIClass(QtGui.QMainWindow, GSMUtility.Ui_MainWindow):
 
 
     def disconnect_gprs(self):
-         GSM_port.write('AT+CIPSHUT'+chr(13))
+        # global tu_veriable
+        # if tu_veriable=='TCP':
+            GSM_port.write('AT+CIPSHUT'+chr(13))
+        # elif tu_veriable=='UDP':
+        #     GSM_port.write('AT+CIPCLOSE'+chr(13))
+
 
     def clear_log(self):
         global Console_Data
         Console_Data=''
         self.SerialConsole.setPlainText(Console_Data)
+
+    def tcp_udp(self,local1):
+        global tu_veriable
+        global portOpen
+        tu_veriable=local1
+
+        if local1=='TCP':
+           self.pushButton_8.setText("Connect TCP")
+        elif local1=='UDP':
+           self.pushButton_8.setText("Connect UDP")
+        elif local1=='Select':
+            self.pushButton_8.setEnabled(False)
+            self.pushButton_8.setText("Connect")
+
+    def ftp_server(self,data6):
+        global ftp_server_name
+        ftp_server_name=data6
+
+    def username(self,data7):
+        global user_name
+        user_name=data7
+
+    def password(self,data8):
+        global ftp_password
+        ftp_password=data8
+
+    def file_name(self,data9):
+        global ftp_file_name
+        ftp_file_name=data9
+
+    def directry(self,data10):
+        global ftp_directry
+        ftp_directry=data10
+
+    def ftp_apn(self,data11):
+        global apn
+        apn=data11
+
+    def ftp_connect(self):
+        global apn
+        global ftp_server_name
+        global user_name
+        global ftp_password
+        GSM_port.write('AT+SAPBR=3,1,"Contype","GPRS"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+SAPBR=3,1,"APN","'+str(apn)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+SAPBR =1,1' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+SAPBR=2,1' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPCID=1' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPSERV="'+str(ftp_server_name)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPUN="'+str(user_name)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPPW="'+str(ftp_password)+'"' + chr(13))
+    def ftp_disconnect(self):
+        GSM_port.write('AT+FTPPUT=2,0' + chr(13))
+
+
+    def ftp_push(self):
+
+        global ftp_file_name
+        global ftp_directry
+        GSM_port.write('AT+FTPPUTNAME="'+str(ftp_file_name)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPPUTPATH="'+str(ftp_directry)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPPUT=1' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPPUT=2,10' + chr(13))
+        time.sleep(1)
+        GSM_port.write(str(self.plainTextEdit_4.toPlainText()) + chr(13))
+
+
+
+    def ftp_get(self):
+        global ftp_file_name
+        global ftp_directry
+        GSM_port.write('AT+FTPGETNAME="'+str(ftp_file_name)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPGETPATH="'+str(ftp_directry)+'"' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPGET=1' + chr(13))
+        time.sleep(1)
+        GSM_port.write('AT+FTPGET=2,1024' + chr(13))
+        time.sleep(1)
+
 
 
 class WorkThread(QtCore.QThread):
@@ -312,6 +442,7 @@ class WorkThread(QtCore.QThread):
 
     def run(self):
         global portOpen
+
         while True:
             while portOpen:
                 global c
